578aa1762bc9942a1422f87d0b81b0847f83a2f2
Oscar Castillo <del.castillo.oscar.a@gmail.com>
2021-06-07 19:44:07 +0200

    test precommit

--- manage.py
+++ manage.py
@@ -5,11 +5,14 @@ import sys
 
 
 def main():
+
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devsecops.settings')
     try:
+
         from django.core.management import execute_from_command_line
     except ImportError as exc:
+
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
@@ -19,6 +22,7 @@ def main():
 
 
 if __name__ == '__main__':
+
     main()
 
 # esto es un comentario
